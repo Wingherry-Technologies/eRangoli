e8ceb9c913babc508b9fd7ba9c14ebb25622aa05
RohanWingherry <rohan.wingherry@gmail.com>
2025-12-19 12:45:56 +0530

    master

--- javascript/topCategory.js
+++ javascript/topCategory.js
@@ -1,35 +1,35 @@
 // Carousel Functionality
 
-let index = 0;
-const slide = document.querySelector('.carousel-slide');
-const images = document.querySelectorAll('.carousel-slide img');
-const dots = document.querySelectorAll('.dot');
+// let index = 0;
+// const slide = document.querySelector('.carousel-slide');
+// const images = document.querySelectorAll('.carousel-slide img');
+// const dots = document.querySelectorAll('.dot');
 
-function showSlide(i) {
-    if (i >= images.length) index = 0;
-    if (i < 0) index = images.length - 1;
+// function showSlide(i) {
+//     if (i >= images.length) index = 0;
+//     if (i < 0) index = images.length - 1;
 
-    slide.style.transform = `translateX(${-index * 100}%)`;
+//     slide.style.transform = `translateX(${-index * 100}%)`;
 
-    dots.forEach(dot => dot.classList.remove('active'));
-    dots[index].classList.add('active');
-}
+//     dots.forEach(dot => dot.classList.remove('active'));
+//     dots[index].classList.add('active');
+// }
 
-dots.forEach((dot, i) => {
-    dot.onclick = () => {
-        index = i;
-        showSlide(index);
-    };
-});
+// dots.forEach((dot, i) => {
+//     dot.onclick = () => {
+//         index = i;
+//         showSlide(index);
+//     };
+// });
 
-// Auto slide every 3 seconds
-setInterval(() => {
-    index++;
-    showSlide(index);
-}, 3000);
+// // Auto slide every 3 seconds
+// setInterval(() => {
+//     index++;
+//     showSlide(index);
+// }, 3000);
 
-// Initial render
-showSlide(index);
+// // Initial render
+// showSlide(index);
 
 
 // Top Category Carousel
