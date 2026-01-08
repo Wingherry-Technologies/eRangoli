056fe244a4f79cf10107e046a7478f4339ddeff9
RohanWingherry <rohan.wingherry@gmail.com>
2026-01-08 17:05:28 +0530

    add

--- javascript/vandorAddProduct.js
+++ javascript/vandorAddProduct.js
@@ -1,3 +1,28 @@
+// Navigation Bar Interaction
+// HAMBURGER OPEN/CLOSE
+const hamburger = document.querySelector(".hamburger-menu");
+var mobileMenu = document.getElementById("mobile-menu");
+var hamberMenuIcon=document.querySelector("#hamburger-menu>img");
+var mobileBack=document.querySelector(".mobile-back-button");
+
+
+hamburger?.addEventListener("click", () => {
+  mobileMenu.classList.toggle("menu-open");
+  // Toggle hamburger icon
+  if (mobileMenu.classList.contains("menu-open")) {
+    hamberMenuIcon.src = "../assets/master/X.svg";
+    document.querySelector(".bottom-nav").style.display="none"
+    document.querySelector("body").style.overflow="hidden"
+    window.scrollTo(0, 0);
+    mobileBack.style.display="none"
+  }
+  else {
+    hamberMenuIcon.src = "../assets/master/List.svg";
+    document.querySelector("body").style.overflow="auto";
+    document.querySelector(".bottom-nav").style.display="flex";
+    mobileBack.style.display="flex"
+  }
+});
 const categoryFields = {
   Footwear: [
     {
@@ -924,7 +949,7 @@ addressCard.className = "address-card";
 addressCard.innerHTML = `
                       <div class="use-address">
                     <input type="checkbox" class="address-checkbox">
-                     <span>use this address details</span>   
+                     <span>Use this address details</span>   
                     </div>
   <div class="address-content">
       <div class="address-name">Address ${addressCount}</div>
