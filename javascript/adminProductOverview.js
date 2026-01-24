db8df24db63f333a6f477a2dd1c2779950dd21ba
RohanWingherry <rohan.wingherry@gmail.com>
2026-01-24 21:33:46 +0530

    product

--- javascript/adminProductOverview.js
+++ javascript/adminProductOverview.js
@@ -1,3 +1,29 @@
+// Navigation Bar Interaction
+// HAMBURGER OPEN/CLOSE
+const hamburger = document.querySelector(".hamburger-menu");
+var mobileMenu = document.getElementById("mobile-menu");
+var hamberMenuIcon=document.querySelector("#hamburger-menu>img");
+var mobileBack=document.querySelector(".mobile-back-button");
+
+
+hamburger?.addEventListener("click", () => {
+  mobileMenu.classList.toggle("menu-open");
+  // Toggle hamburger icon
+  if (mobileMenu.classList.contains("menu-open")) {
+    hamberMenuIcon.src = "../assets/master/X.svg";
+    document.querySelector(".bottom-nav").style.display="none"
+    document.querySelector("body").style.overflow="hidden"
+    window.scrollTo(0, 0);
+    mobileBack.style.display="none"
+  }
+  else {
+    hamberMenuIcon.src = "../assets/master/List.svg";
+    document.querySelector("body").style.overflow="auto";
+    document.querySelector(".bottom-nav").style.display="flex";
+    mobileBack.style.display="flex"
+  }
+});
+
 // Thumbnail image switch
 document.addEventListener("DOMContentLoaded", () => {
   const thumbnails = document.querySelectorAll(".APOThumbnailItem");
@@ -106,3 +132,10 @@ APOremoveYes?.addEventListener("click", () => {
 
   currentAction = "";
 });
+
+document.querySelector(".sidebar-main-vendor ul>li:nth-child(2)").classList.add("sidebar-active");
+document.querySelector(".sidebar-main-vendor ul>ul>li:nth-child(1)").classList.add("submenu-active-highlight");
+document.querySelector(".sidebar-main-vendor ul>ul").classList.add("active");
+
+document.querySelector("#account-menu .mobile-dropdown:nth-child(2) .dropdown-header").classList.add("dropdown-header-active");
+document.querySelector("#account-menu .mobile-dropdown:nth-child(2)").classList.add("active-mobile-submenu");
