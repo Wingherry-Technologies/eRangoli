aab4d20a4423aaa34bf36f61f66e3f315ad8457f
RohanWingherry <rohan.wingherry@gmail.com>
2026-02-04 18:44:23 +0530

    all

--- javascript/adminSalesCoupons.js
+++ javascript/adminSalesCoupons.js
@@ -1,3 +1,25 @@
+// Navigation Bar Interaction
+// HAMBURGER OPEN/CLOSE
+const hamburger = document.querySelector(".hamburger-menu");
+var mobileMenu = document.getElementById("mobile-menu");
+var hamberMenuIcon=document.querySelector("#hamburger-menu>img");
+
+
+hamburger?.addEventListener("click", () => {
+  mobileMenu.classList.toggle("menu-open");
+  // Toggle hamburger icon
+  if (mobileMenu.classList.contains("menu-open")) {
+    hamberMenuIcon.src = "../assets/master/X.svg";
+    document.querySelector(".bottom-nav").style.display="none"
+    document.querySelector("body").style.overflow="hidden"
+    window.scrollTo(0, 0);
+  }
+  else {
+    hamberMenuIcon.src = "../assets/master/List.svg";
+    document.querySelector("body").style.overflow="auto";
+    document.querySelector(".bottom-nav").style.display="flex";
+  }
+});
 document.addEventListener("DOMContentLoaded", function () {
   const couponBtn = document.querySelector(".create-coupon-btn");
   if (couponBtn) {
