c8876f386d970143f11b787f150265757e1c928d
RohanWingherry <rohan.wingherry@gmail.com>
2026-02-20 17:42:53 +0530

    add

--- javascript/developerPMRecentlyAdded.js
+++ javascript/developerPMRecentlyAdded.js
@@ -279,3 +279,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+document.querySelector(".sidebar-main-vendor > article > ul >li:nth-of-type(2)").classList.add("sidebar-active");
+document.querySelector(".sidebar-main-vendor ul>ul:nth-of-type(1)").classList.add("active");
+document.querySelector(".sidebar-main-vendor ul>ul:nth-of-type(1)>li:nth-child(1)").classList.add("submenu-active-highlight");
+
+
+document.querySelector("#account-menu .mobile-dropdown:nth-child(2) .dropdown-header").classList.add("dropdown-header-active");
+document.querySelector("#account-menu .mobile-dropdown:nth-child(2)").classList.add("active-mobile-submenu");
+document.querySelector("#account-menu .mobile-dropdown:nth-child(2) li:nth-child(1)").classList.add("submenu-active-page");
