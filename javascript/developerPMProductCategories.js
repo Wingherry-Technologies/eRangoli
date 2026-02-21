0c5979e420920ef045489e651fe5965d829757a4
RohanWingherry <rohan.wingherry@gmail.com>
2026-02-21 10:59:51 +0530

    add

--- javascript/developerPMProductCategories.js
+++ javascript/developerPMProductCategories.js
@@ -1,3 +1,26 @@
+// Navigation Bar Interaction
+// HAMBURGER OPEN/CLOSE
+const hamburger = document.querySelector(".hamburger-menu");
+var mobileMenu = document.getElementById("mobile-menu");
+var hamberMenuIcon=document.querySelector("#hamburger-menu>img");
+
+
+hamburger?.addEventListener("click", () => {
+  mobileMenu.classList.toggle("menu-open");
+  // Toggle hamburger icon
+  if (mobileMenu.classList.contains("menu-open")) {
+    hamberMenuIcon.src = "../assets/master/X.svg";
+    document.querySelector(".bottom-nav").style.display="none"
+    document.querySelector("body").style.overflow="hidden"
+    window.scrollTo(0, 0);
+  }
+  else {
+    hamberMenuIcon.src = "../assets/master/List.svg";
+    document.querySelector("body").style.overflow="auto";
+    document.querySelector(".bottom-nav").style.display="flex";
+  }
+});
+
 document.querySelectorAll(".APRAfaq-question").forEach((btn) => {
   btn.addEventListener("click", () => {
     btn.closest(".APRAfaq-item").classList.toggle("active");
@@ -13,3 +36,13 @@ document
   ?.addEventListener("click", function () {
     window.location.href = "../html/developerPMCreateNewCategories.html";
   });
+
+document.querySelector(".sidebar-main-vendor > article > ul >li:nth-of-type(2)").classList.add("sidebar-active");
+document.querySelector(".sidebar-main-vendor ul>ul:nth-of-type(1)").classList.add("active");
+document.querySelector(".sidebar-main-vendor ul>ul:nth-of-type(1)>li:nth-child(2)").classList.add("submenu-active-highlight");
+
+
+document.querySelector("#account-menu .mobile-dropdown:nth-child(2) .dropdown-header").classList.add("dropdown-header-active");
+document.querySelector("#account-menu .mobile-dropdown:nth-child(2)").classList.add("active-mobile-submenu");
+document.querySelector("#account-menu .mobile-dropdown:nth-child(2) li:nth-child(2)").classList.add("submenu-active-page");
+
